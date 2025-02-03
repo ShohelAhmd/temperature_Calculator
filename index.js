ace951d09b5864a6171e7cba1cce086a7074aa2a
shohel-ahmed <shohelahmed27.sa@gmail.com>
2025-02-03 10:33:35 +0800

    added Fahrenheight

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ var farn= prompt("Enter the Fahrenheight : ");
 
 var cels= (farn-32) * (5/9);
 document.write("Celsius = "+ cels);
+
+
+var cel= prompt("Enter Celsius : ");
+var fr= (cel*(9/5)+32);
+document.write("Fahrenheight : "+fr);
